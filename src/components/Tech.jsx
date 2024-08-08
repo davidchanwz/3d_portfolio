fad276801e797cdc032bc75709b8cf5010c6e421
David Chan <davidchan@Davids-Macbook-Air.local>
2024-08-09 03:03:32 +0800

    fixed mobile styling of 3d com

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,15 +4,33 @@ import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
+import { fadeIn, textVariant } from "../utils/motion";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+
+
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
+
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText}`}>
+          My tools
+        </p>
+        <h2 className={`${styles.sectionHeadText} pb-5`}>
+          Technology.
+        </h2>
+      </motion.div>
+
+
+      <div className='flex flex-row flex-wrap justify-center gap-10'>
+        {technologies.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -22,9 +22,9 @@ const Computers = ({ isMobile }) => {
         <pointLight intensity={1} />
         <primitive
           object={computer.scene}
-          scale={isMobile ? 0.7 : 0.75}
-          position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-          rotation={[-0.01, -0.2, -0.1]}
+          scale={isMobile ? 0.3 : 0.75}
+          position={isMobile ? [0, -1, -0.55] : [0, -3.25, -1.5]}
+          rotation={isMobile ? [-0.1,-0.2,-0.1] : [-0.01, -0.2, -0.1]}
           decay={0}
         />
       </mesh>
