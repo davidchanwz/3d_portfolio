a30b3439df58c45b99de293f43dabdfd24834d4e
David Chan <davidchanwz@gmail.com>
2025-01-25 19:01:23 +0800

    edit hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,10 +19,9 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span
             className="text-[#915eff]">David</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2
-            text-white-100`}>
-            I develop cool stuff with chatgpt <br
-              className="sm:block hidden" />
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop cool stuff for <span className="line-through">fun</span> <span>work</span>
+            <br className="sm:block hidden" />
           </p>
 
         </div>
@@ -37,16 +36,16 @@ const Hero = () => {
           <div className="w-[35px] h-[64px] rounded-3xl
           border-4 border-secondary flex justify-center
           items-start">
-            <motion.div 
-            animate={{
-              y:[0, 24, 0]
-            }}
-            transition={{
-              duration:1.5,
-              repeat: Infinity,
-              repreatType: 'loop'
-            }}
-            className="w-3 h-3 rounded-full
+            <motion.div
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repreatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full
             bg-secondary mb-1"/>
           </div>
         </a>
